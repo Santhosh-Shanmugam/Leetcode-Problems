d9e6e1436a4c78313e83adfe2b5231ee5aaf571f
Santhosh S <santhoshs192005@gmail.com>
2025-12-30 16:15:45 +0530

    Time: 1 ms (100%), Space: 44.1 MB (41.95%) - LeetHub

---
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addMinimum(String word) {
+        int i=0,ans=0;
+        while(i < word.length())
+        {
+            if (i < word.length() && word.charAt(i) == 'a') i++;
+            else ans++;
+
+            if (i < word.length() && word.charAt(i) == 'b') i++;
+            else ans++;
+
+            if (i < word.length() && word.charAt(i) == 'c') i++;
+            else ans++;
+        }
+        return ans;
+    }
+}
